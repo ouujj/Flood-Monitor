dafddda998be9c61d745e7b4b58f5a5158f6cb4d
Saranyu Ngaosri <39403998+ouujj@users.noreply.github.com>
2019-08-01 18:43:27 +0700

    Read bit Level

--- Flood_Monitor/Flood_Monitor.ino
+++ Flood_Monitor/Flood_Monitor.ino
@@ -1,6 +1,10 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #define flood_Level_pin A0
+#define Level_bit1 14
+#define Level_bit2 12
+#define Level_bit3 13
+
 byte last_suit , current_suit= 0;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
@@ -8,7 +12,9 @@ void setup()
 { 
   Serial.begin (115200);
   pinMode(flood_Level_pin,INPUT);
-
+  pinMode(Level_bit1,INPUT_PULLUP);
+  pinMode(Level_bit2,INPUT_PULLUP);
+ pinMode(Level_bit3,INPUT_PULLUP);
   //Scan I2C Device Address
   Serial.println ();
   Serial.println ("I2C scanner. Scanning ...");
@@ -42,8 +48,9 @@ void setup()
 }
 
 void loop() {
-
-  ReadFloodRLevel();
+  int Levelb1 = digitalRead(Level_bit1);
+  Serial.println(Levelb1);
+  ReadFloodRLevelBit();
   delay(100);
 
 }
@@ -68,8 +75,7 @@ if(last_suit != current_suit){
   lcd.setCursor(2, 1);
   lcd.print(siut);
   Serial.println("FLOOD LEVEL: "+siut);
-  last_suit = current_suit;
-  
+  last_suit = current_suit;  
 }
 
 
@@ -97,5 +103,37 @@ void ReadFloodRLevel(){
       situation("Danger");
       current_suit = 3;
      }
+  
+}
+void ReadFloodRLevelBit(){
+  
+     int Levelb1 = digitalRead(Level_bit1);
+    byte Levelb2 = digitalRead(Level_bit2)*2;
+     byte Levelb3 = digitalRead(Level_bit3)*4;
+    byte Level = Levelb1+Levelb2+Levelb3;
+   
+     Serial.print("Rew Level: ");
+     Serial.println(Level);
+     
+     if( Level == 0){
+      situation("Normal");
+      current_suit = 0;
+      }
+     else if(Level == 1 ) {
+        situation("a Few ");
+        current_suit = 1;
+     }
+     else if(Level == 3) {
+        situation("hard");
+        current_suit = 2;
+      }
+     else if(Level == 7)  {
+      situation("Danger");
+      current_suit = 3;
+     }
+     else{
+      situation("Error");
+      current_suit = 4;
+      }
   
 }
